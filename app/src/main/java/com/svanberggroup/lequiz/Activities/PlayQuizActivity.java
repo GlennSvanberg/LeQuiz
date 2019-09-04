8799bbeef73da5e88b9de899f35026e5017e7c52
Glenn Svanberg <signeratsvanberg@gmail.com>
2019-09-04 12:03:38 +0200

    Load quiz in PlayQuizFragment

--- app/src/main/java/com/svanberggroup/lequiz/Activities/PlayQuizActivity.java
+++ app/src/main/java/com/svanberggroup/lequiz/Activities/PlayQuizActivity.java
@@ -8,18 +8,25 @@ import androidx.fragment.app.Fragment;
 import com.svanberggroup.lequiz.Fragements.PlayQuizFragment;
 import com.svanberggroup.lequiz.R;
 
+import java.util.UUID;
+
+
 public class PlayQuizActivity extends SingleFragmentActivity{
 
+    public static final String EXTRA_QUIZ_ID = "com.svanberggroup.lequiz.quiz_id";
 
     @Override
-    protected Fragment createFragment() {
-        return new PlayQuizFragment();
+    protected Fragment createFragment(){
+        UUID quizId = (UUID) getIntent().getSerializableExtra(EXTRA_QUIZ_ID);
+        return PlayQuizFragment.newInstance(quizId);
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
     }
+
 }
 
--- app/src/main/java/com/svanberggroup/lequiz/Fragements/PlayQuizFragment.java
+++ app/src/main/java/com/svanberggroup/lequiz/Fragements/PlayQuizFragment.java
@@ -4,21 +4,70 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
+import com.svanberggroup.lequiz.Models.Quiz;
 import com.svanberggroup.lequiz.R;
+import com.svanberggroup.lequiz.ViewModels.QuizViewModel;
 
+import java.util.UUID;
 import java.util.zip.Inflater;
 
 public class PlayQuizFragment extends Fragment {
+
+    private Quiz mQuiz;
+
+    private static final String ARG_QUIZ_ID = "quiz_id";
+
+    private QuizViewModel mQuizViewModel;
+
+    private TextView textVeiw;
+
+
+    public static PlayQuizFragment newInstance(UUID quizId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_QUIZ_ID, quizId);
+
+        PlayQuizFragment fragment = new PlayQuizFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mQuizViewModel = ViewModelProviders.of(this).get(QuizViewModel.class);
+
+
+        if(getArguments().getSerializable(ARG_QUIZ_ID) != null) {
+            mQuiz = new Quiz();
+            mQuiz.setId((UUID) getArguments().getSerializable(ARG_QUIZ_ID));
+
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_play_quiz, container, false);
+
+        textVeiw = view.findViewById(R.id.textView);
+
+
+        mQuizViewModel.getQuiz(mQuiz.getId().toString()).observe(getActivity(), new Observer<Quiz>() {
+            @Override
+            public void onChanged(Quiz quiz) {
+                mQuiz = quiz;
+                textVeiw.setText(mQuiz.getTitle());
+            }
+        });
 
         return view;
     }
